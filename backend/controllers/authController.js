7b3bcd6d6d0322b603fc2d90a574201053049f33
rahmanjihad <jillurjihad@gmail.com>
2026-03-15 20:11:59 +0600

    MVC folders added

---
+++ backend/controllers/authController.js
@@ -0,0 +1,36 @@
+const User = require("../models/User");
+const bcrypt = require("bcryptjs");
+
+exports.registerUser = async (req, res) => {
+  try {
+
+    const { name, email, password, role } = req.body;
+
+    // check if user exists
+    const userExists = await User.findOne({ email });
+
+    if (userExists) {
+      return res.status(400).json({ message: "User already exists" });
+    }
+
+    // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    // create user
+    const user = new User({
+      name,
+      email,
+      password: hashedPassword,
+      role
+    });
+
+    await user.save();
+
+    res.status(201).json({
+      message: "User registered successfully"
+    });
+
+  } catch (error) {
+    res.status(500).json({ error: error.message });
+  }
+};
